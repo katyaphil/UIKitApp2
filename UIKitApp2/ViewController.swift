b2ee810c58ac1dd8c2cdd876634d46637a349312
Filipovich Katerina <k.filipovich@city-mobil.ru>
2020-10-16 23:05:23 +0300

    Initial Commit

---
+++ UIKitApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitApp2
+//
+//  Created by k.filipovich on 16/10/2020.
+//  Copyright © 2020 k.filipovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

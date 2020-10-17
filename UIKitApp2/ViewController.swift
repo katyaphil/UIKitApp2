2b2ed12783617a42afcd636d3a3ad7d88ad6fa05
Filipovich Katerina <k.filipovich@city-mobil.ru>
2020-10-18 01:23:17 +0300

    fix connections

--- UIKitApp2/ViewController.swift
+++ UIKitApp2/ViewController.swift
@@ -10,58 +10,75 @@ import UIKit
 
 class ViewController: UIViewController {
     
- @IBOutlet var mainLable: UILabel!
-    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var mSegment: UISegmentedControl!
+    
+    
+    @IBOutlet var mLabel: UILabel!
+    
     @IBOutlet var slider: UISlider!
+    
     @IBOutlet var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        mainLable?.font = mainLable.font.withSize(35)
-        mainLable?.textAlignment = .center
-        mainLable?.numberOfLines = 2
+  //      mLable?.font = mLable.font.withSize(35)
+//        mLable?.textAlignment = .center
+ //       mLable?.numberOfLines = 2
         
-        segmentedControl?.insertSegment(withTitle: "Third", at: 2, animated: false)
-        slider.value = 1
-        slider.maximumValue = 0
-        slider.maximumValue = 1
-        slider.minimumTrackTintColor = .yellow
-        slider.maximumTrackTintColor = .red
-        slider.thumbTintColor = .blue
-          mainLable?.text = String(slider.value)
+//        segmentedControl?.insertSegment(withTitle: "Third", at: 2, animated: false)
+//        slider.value = 1
+//        slider.maximumValue = 0
+//        slider.maximumValue = 1
+//        slider.minimumTrackTintColor = .yellow
+//        slider.maximumTrackTintColor = .red
+//        slider.thumbTintColor = .blue
+//          mainLable?.text = String(slider.value)
     }
-  
-    @IBAction func segmentedControlAction() {
-        switch segmentedControl?.selectedSegmentIndex {
+    
+    @IBAction func segmentAction() {
+        switch mSegment.selectedSegmentIndex {
         case 0:
-            mainLable?.text = "the first is selected"
-            mainLable?.textColor = .red
+            mLabel?.text = "the first is selected"
+            mLabel?.textColor = .red
         case 1:
-            mainLable?.text = "the second is selected"
-             mainLable?.textColor = .yellow
-        case 2:
-            mainLable?.text = "the third is selected"
-            mainLable?.textColor = .green
+            mLabel?.text = "the second is selected"
+            mLabel?.textColor = .yellow
+            
         default:
             break
         }
     }
-    
     @IBAction func sliderAction() {
-        let currentValue = CGFloat(slider.value)
-        mainLable?.text = String(slider.value)
-        view.backgroundColor = view.backgroundColor?.withAlphaComponent(currentValue)
+        
+                let currentValue = CGFloat(slider.value)
+                mLabel?.text = String(slider.value)
+                view.backgroundColor = view.backgroundColor?.withAlphaComponent(currentValue)
     }
-    @IBAction func doneButtonPressed() {
-        guard let inputText = textField.text, !inputText.isEmpty else {
-            print("Text field is empty")
-            return
-        }
-        if let number = Double(inputText) {
-            print("Wrong format")
-            return
-        }
-        mainLable.text = inputText
+    
+    @IBAction func buttonAction() {
+                guard let inputText = textField.text, !inputText.isEmpty else {
+                    print("Text field is empty")
+                    return
+                }
+                if let number = Double(inputText) {
+                    print("Wrong format")
+                    return
+                }
+        mLabel.text = inputText
     }
+    
+
+//    @IBAction func doneButtonPressed() {
+//        guard let inputText = textField.text, !inputText.isEmpty else {
+//            print("Text field is empty")
+//            return
+//        }
+//        if let number = Double(inputText) {
+//            print("Wrong format")
+//            return
+//        }
+//        mainLable.text = inputText
+//    }
 }
 
